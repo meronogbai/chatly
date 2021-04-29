85a33e0464d1265aac96972ff18f6b0ac97aa6db
Meron Ogbai <okbaymeron@gmail.com>
2021-04-29 03:38:58 +0300

    Add html

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log('Server is up!')
 });
+
+app.use(express.static('public'));
