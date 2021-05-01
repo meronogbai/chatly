4fc1560f47e088bd791297aae78376b1d8157d65
Meron Ogbai <okbaymeron@gmail.com>
2021-05-01 10:33:30 +0300

    Add code to display and post messages to db

---
+++ public/app.js
@@ -0,0 +1,26 @@
+const messageForm = document.getElementById('messageForm');
+const messagesDiv = document.getElementById('messagesDiv');
+
+
+fetch('/messages')
+  .then(response => response.json())
+  .then(messages => {
+    messages.forEach(message => {
+      messagesDiv.innerHTML += `<h4>${message.name}</h4><p>${message.message}</p>`;
+    });
+  });
+
+messageForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const body = JSON.stringify({
+    name: document.getElementById('name').value,
+    message: document.getElementById('message').value,
+  });
+  fetch('/messages', {
+    method: 'post',
+    body,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+});
